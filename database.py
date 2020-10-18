78317045721935b715bd595c81a496c99c8a4b0d
Elijah <elijahlaruffa@gmail.com>
2020-10-18 16:50:55 -0400

    Update database.py

--- database.py
+++ database.py
@@ -5,33 +5,43 @@ class Database():
         self.mongo = PyMongo(app)
 
     def set_username(self, id, username):
-        self.mongo.db.user.insert_One({"username": username})
+        self.mongo.db.user.update_one({"id": id}, {"$set": {
+            "username": username
+        }
+        })
 
     def get_username(self, id, username):
-        return self.mongo.db.user.findOne({"username": username})
+        return self.mongo.db.user.find({"id": id, "username": username})
 
     def set_password(self, id, password):
-        self.mongo.db.user.insert_One({"password": password})
+        self.mongo.db.user.update_one({"id": id}, {"$set": {
+            "password": password
+        }
+        })
 
     def get_password(self, id, password):
-        return self.mongo.db.user.findOne({"password": password})
+        return self.mongo.db.user.find({"id": id, "password": password})
 
     def set_location(self, id, location):
-        self.mongo.db.user.insert_One({"location": location})
+        self.mongo.db.user.update_one({"id": id}, {"$set": {
+            "location": location
+        }
+        })
 
     def get_location(self, id, location):
-        return self.mongo.db.user.findOne({"location": location})
+        return self.mongo.db.user.find({"id": id, "location": location})
 
     def set_phone(self, id, phone):
-        self.mongo.db.user.insert_One({"phone": phone})
+        self.mongo.db.user.update_one({"id": id}, {"$set": {
+            "phone": phone
+        }
+        })
 
     def get_phone(self, id, phone):
-        return self.mongo.db.user.findOne({"phone": phone})
+        return self.mongo.db.user.find({"id": id, "phone": phone})
 
     def add_user(self, user):
         self.set_username(user.id, user.username)
         self.set_password(user.id, user.password)
         self.set_location(user.id, user.location)
         self.set_phone(user.id, user.phone)
-
-
