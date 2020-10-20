2a44104bed5f04a31ea6b1fc46547e91658ce679
Derek Santos <derek@dyrenex.com>
2020-10-20 18:17:24 -0400

    Added more tests for database

--- tests/test_database.py
+++ tests/test_database.py
@@ -11,44 +11,236 @@ mongo = PyMongo(app)
 
 class TestDatabase(unittest.TestCase):
     def test_set_username(self):
+        
         # Creating database object
         database = Database(app)
 
         # Creating new User object
         new_user = User(
             None, 
-            username="derek", 
-            password="derek",
-            firstname="derek",
-            lastname="santos",
+            username="test", 
+            password="password",
+            firstname="first",
+            lastname="last",
             location="Earth",
             phone=1234567890)
+
+        # Remove test user
+        while database.get_user_class_by_username(new_user.username):
+            db_user = database.get_user_class_by_username(new_user.username)
+            database.remove_user(db_user.id)
 
         # Adding new user
         database.add_user(new_user)
 
         # Geting the new user by their username 
-        db_get = database.get_user_class_by_username(new_user.username)
+        db_user_1 = database.get_user_class_by_username(new_user.username)
 
         # Setting our current user object's id as mongodb id
-        new_user.id = db_get.id
+        new_user.id = db_user_1.id
 
         # Checking if user objects are the same through their dicts
-        
-        self.assertTrue (db_get.as_dict() == new_user.as_dict())
+        self.assertTrue (db_user_1.as_dict() == new_user.as_dict())
 
         # Changing new user's name to 'elijah'
         new_user.username = "elijah"
         database.set_username(new_user.id, new_user.username)
 
         # Checking if database updated
-        db_get_2 = database.get_user_class_by_username(new_user.username)
+        db_user_2 = database.get_user_class_by_username(new_user.username)
 
-        self.assertTrue (db_get_2.as_dict() == new_user.as_dict())
+        self.assertTrue (db_user_2.as_dict() == new_user.as_dict())
 
         # Removing temp user from database
-        database.remove_user(db_get_2.id)
-        self.assertTrue(database.get_by_id(db_get_2.id) == None)
+        database.remove_user(db_user_2.id)
+        self.assertTrue(database.get_by_id(db_user_2.id) == None)
+
+    def test_set_password(self): 
+
+        # Creating database object
+        database = Database(app)
+
+        # Creating new User object
+        new_user = User(
+            None, 
+            username="test", 
+            password="password",
+            firstname="first",
+            lastname="last",
+            location="Earth",
+            phone=1234567890)
+
+        # Remove test user
+        while database.get_user_class_by_username(new_user.username):
+            db_user = database.get_user_class_by_username(new_user.username)
+            database.remove_user(db_user.id)
+
+        # Adding new user
+        database.add_user(new_user)
+
+        # Updating user object to database user 
+        new_user = database.get_user_class_by_username(new_user.username)
+
+        # Changing password
+        new_user.password = "newPassword"
+        database.set_password(new_user.id, new_user.password)
+
+        # Checking password
+        db_user = database.get_user_class_by_username(new_user.username)
+        self.assertTrue(db_user.password == new_user.password)
+
+        database.remove_user(db_user.id)
+        self.assertTrue(database.get_user_class_by_username(db_user.id) == None)
+
+    def test_set_location(self): 
+
+        # Creating database object
+        database = Database(app)
+
+        # Creating new User object
+        new_user = User(
+            None, 
+            username="test", 
+            password="password",
+            firstname="first",
+            lastname="last",
+            location="Earth",
+            phone=1234567890)
+
+        # Remove test user
+        while database.get_user_class_by_username(new_user.username):
+            db_user = database.get_user_class_by_username(new_user.username)
+            database.remove_user(db_user.id)
+
+        # Adding new user
+        database.add_user(new_user)
+
+        # Updating user object to database user 
+        new_user = database.get_user_class_by_username(new_user.username)
+
+        # Changing location
+        new_user.location = "newLocation"
+        database.set_location(new_user.id, new_user.location)
+
+        # Checking location
+        db_user = database.get_user_class_by_username(new_user.username)
+        self.assertTrue(db_user.location == new_user.location)
+
+        database.remove_user(db_user.id)
+        self.assertTrue(database.get_user_class_by_username(db_user.id) == None)
+
+
+        def test_set_phone(self): 
+
+            # Creating database object
+            database = Database(app)
+
+            # Creating new User object
+            new_user = User(
+                None, 
+                username="test", 
+                password="password",
+                firstname="first",
+                lastname="last",
+                location="Earth",
+                phone=1234567890)
+
+            # Remove test user
+            while database.get_user_class_by_username(new_user.username):
+                db_user = database.get_user_class_by_username(new_user.username)
+                database.remove_user(db_user.id)
+
+            # Adding new user
+            database.add_user(new_user)
+
+            # Updating user object to database user 
+            new_user = database.get_user_class_by_username(new_user.username)
+
+            # Changing phone
+            new_user.phone = "newPhone"
+            database.set_phone(new_user.id, new_user.phone)
+
+            # Checking phone
+            db_user = database.get_user_class_by_username(new_user.username)
+            self.assertTrue(db_user.phone == new_user.phone)
+
+            database.remove_user(db_user.id)
+            self.assertTrue(database.get_user_class_by_username(db_user.id) == None)
+
+    def test_set_firstname(self): 
+
+        # Creating database object
+        database = Database(app)
+
+        # Creating new User object
+        new_user = User(
+            None, 
+            username="test", 
+            password="password",
+            firstname="first",
+            lastname="last",
+            location="Earth",
+            phone=1234567890)
+
+        # Remove test user
+        while database.get_user_class_by_username(new_user.username):
+            db_user = database.get_user_class_by_username(new_user.username)
+            database.remove_user(db_user.id)
+
+        # Adding new user
+        database.add_user(new_user)
+
+        # Updating user object to database user 
+        new_user = database.get_user_class_by_username(new_user.username)
+
+        # Changing firstname
+        new_user.firstname = "newfirstname"
+        database.set_firstname(new_user.id, new_user.firstname)
+
+        # Checking firstname
+        db_user = database.get_user_class_by_username(new_user.username)
+        self.assertTrue(db_user.firstname == new_user.firstname)
+
+        database.remove_user(db_user.id)
+        self.assertTrue(database.get_user_class_by_username(db_user.id) == None)
+
+    def test_set_lastname(self): 
+
+        # Creating database object
+        database = Database(app)
+
+        # Creating new User object
+        new_user = User(
+            None, 
+            username="test", 
+            password="password",
+            firstname="first",
+            lastname="last",
+            location="Earth",
+            phone=1234567890)
+
+        # Remove test user
+        while database.get_user_class_by_username(new_user.username):
+            db_user = database.get_user_class_by_username(new_user.username)
+            database.remove_user(db_user.id)
+
+        # Adding new user
+        database.add_user(new_user)
+
+        # Updating user object to database user 
+        new_user = database.get_user_class_by_username(new_user.username)
+
+        # Changing lastname
+        new_user.lastname = "newlastname"
+        database.set_lastname(new_user.id, new_user.lastname)
+
+        # Checking lastname
+        db_user = database.get_user_class_by_username(new_user.username)
+        self.assertTrue(db_user.lastname == new_user.lastname)
+
+        database.remove_user(db_user.id)
+        self.assertTrue(database.get_user_class_by_username(db_user.id) == None)
+
 
 
 if __name__ == '__main__':
