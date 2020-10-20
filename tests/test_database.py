9b0825cd850570b9185f4d83b9affdb9d435139d
Derek Santos <derek@dyrenex.com>
2020-10-20 17:51:40 -0400

    Fixed user id issues

--- tests/test_database.py
+++ tests/test_database.py
@@ -15,28 +15,40 @@ class TestDatabase(unittest.TestCase):
         database = Database(app)
 
         # Creating new User object
-        new_user = User(-99.0, username="derek", password="derek")
-        
+        new_user = User(
+            None, 
+            username="derek", 
+            password="derek",
+            firstname="derek",
+            lastname="santos",
+            location="Earth",
+            phone=1234567890)
+
         # Adding new user
         database.add_user(new_user)
 
         # Geting the new user by their username 
-        db_get = database.get_by_username(new_user.username)
-        db_get.pop('_id')
-        self.assertTrue (db_get == new_user.as_dict())
+        db_get = database.get_user_class_by_username(new_user.username)
+
+        # Setting our current user object's id as mongodb id
+        new_user.id = db_get.id
+
+        # Checking if user objects are the same through their dicts
+        
+        self.assertTrue (db_get.as_dict() == new_user.as_dict())
 
         # Changing new user's name to 'elijah'
         new_user.username = "elijah"
         database.set_username(new_user.id, new_user.username)
 
         # Checking if database updated
-        db_get_2 = database.get_by_username(new_user.username)
-        db_get_2.pop('_id')
-        self.assertTrue (db_get_2 == new_user.as_dict())
+        db_get_2 = database.get_user_class_by_username(new_user.username)
+
+        self.assertTrue (db_get_2.as_dict() == new_user.as_dict())
 
         # Removing temp user from database
-        database.remove_user(1)
-        self.assertTrue(database.get_by_id(1) == None)
+        database.remove_user(db_get_2.id)
+        self.assertTrue(database.get_by_id(db_get_2.id) == None)
 
 
 if __name__ == '__main__':
