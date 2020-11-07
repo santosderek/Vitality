176f57a6014237cc3fe58b2d3328c596bede5084
Derek Santos <derek@dyrenex.com>
2020-11-07 17:30:02 -0500

    Updated the user class according to the UML.

--- vitality/user.py
+++ vitality/user.py
@@ -1,5 +1,18 @@
 class User:
-    def __init__(self, _id: str, username: str, password: str, name: str = None, location: str = None, phone: int = None):
+    def __init__(self,
+                 _id: str,
+                 username: str,
+                 password: str,
+                 name: str = None,
+                 location: str = None,
+                 phone: int = None,
+                 body_type: str = None,
+                 body_fat: str = None,
+                 height: str = None,
+                 weight: str = None,
+                 exp: str = None,
+                 goal_weight: str = None,
+                 goal_body_fat: str = None):
         """Constructor for Trainee class."""
         self.__dict__.update(dict(
             _id=_id,
@@ -7,7 +20,14 @@ class User:
             password=password,
             name=name,
             location=location,
-            phone=phone
+            phone=phone,
+            body_type=body_type,
+            body_fat=body_fat,
+            height=height,
+            weight=weight,
+            exp=exp,
+            goal_weight=goal_weight,
+            goal_body_fat=goal_body_fat
         ))
 
     def as_dict(self):
