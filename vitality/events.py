5aca0dc9a63c7d9b6ede62115d764358e970a19e
Elijah <elijahlaruffa@gmail.com>
2020-12-04 13:31:10 -0500

    Create events.py

---
+++ vitality/events.py
@@ -0,0 +1,13 @@
+import datetime
+class Event:
+    def __init__(self, _id: str, creator_id: str, date: datetime, title: str, description: str,):
+        """Constructor for Workout class."""
+        self._id = _id
+        self.creator_id = creator_id
+        self.date = date
+        self.title = title
+        self.description = description
+
+    def as_dict(self):
+        """Returns all attributes of the Workout class as a dictionary."""
+        return dict(self.__dict__)
