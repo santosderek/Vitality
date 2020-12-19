8b89d108716b31228222231ab827dfe3860b174a
Derek Santos <derek@dyrenex.com>
2020-12-19 10:02:24 -0500

    Adding defaults.

--- tests/test_youtube.py
+++ tests/test_youtube.py
@@ -1,4 +1,4 @@
-from vitality.youtube import Youtube
+from vitality.youtube import *
 from os import environ, getenv
 from os.path import exists
 from vitality.settings import GOOGLE_MAPS_KEY
@@ -15,3 +15,10 @@ def test_search_topic():
     assert len(response) == 6
     assert 'items' in response
     assert 'etag' in response
+
+def test_fallback_dicts():
+    assert 'kind' in DEFAULT_FALL_BACK_WORKOUT_DICT
+    assert 'etag' in DEFAULT_FALL_BACK_WORKOUT_DICT
+    assert 'nextPageToken' in DEFAULT_FALL_BACK_WORKOUT_DICT
+    assert 'regionCode' in DEFAULT_FALL_BACK_WORKOUT_DICT
+    assert 'pageInfo' in DEFAULT_FALL_BACK_WORKOUT_DICT
